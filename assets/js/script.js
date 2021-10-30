8390933b45a90f3d2e7e55c9a52d0b39b21f6e1b
ening <ening6649@yahoo.com>
2021-10-30 11:13:49 -0400

    added moment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,29 @@
-var rightNow = new Date(); // don't worry about this "new" keyword yet
+// var rightNow = new Date(); // don't worry about this "new" keyword yet
 
-console.log(rightNow); // the result would look like a string...
-console.log(typeof rightNow); // but its data type is an "object"
+// console.log(rightNow); // the result would look like a string...
+// console.log(typeof rightNow); // but its data type is an "object"
+
+// var currentTime = moment();
+// californiaTime.textContent = currentTime.format('MMM DD, YYYY - hh:mm:ss a')
+// newyorkTime.textContent = currentTime.add(3,'hours').format(blah)
+// moment().format ("ddd,hA");
+
+// var rightNow = moment().format("MMMM Do, YYYY - hh:mm:ss a");
+// console.log(rightNow);
+
+// var tomorrow = moment().add(1, "day").format("dddd, MM-D-YYYY [at] hh:mm:ss A");
+// console.log(tomorrow);
+
+// var pastDate = moment("12-01-1999", "MM-DD-YYYY").format("dddd, MM/DD/YY");
+// console.log(pastDate);
 
 var currentTime = moment();
-californiaTime.textContent = currentTime.format('MMM DD, YYYY - hh:mm:ss a')
-newyorkTime.textContent = currentTime.add(3,'hours').format(blah)
-moment().format ("ddd,hA");
+let currentTimeEl = document.createElement('p');
+let containerEl = document.createElement('div');
+// how to append to the div class container 
+body.appendChild(containerEl);
+containerEl.appendChild(currentTimeEl);
+currentTimeEl.textContent = currentTime.format('MMM DD, YYYY')
 
-var rightNow = moment().format("MMMM Do, YYYY - hh:mm:ss a");
-console.log(rightNow);
 
-var tomorrow = moment().add(1, "day").format("dddd, MM-D-YYYY [at] hh:mm:ss A");
-console.log(tomorrow);
 
-var pastDate = moment("12-01-1999", "MM-DD-YYYY").format("dddd, MM/DD/YY");
-console.log(pastDate);
